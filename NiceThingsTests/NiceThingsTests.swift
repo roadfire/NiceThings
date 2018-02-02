82ca20ddf4af98cbdbdf62fa0295752f4c8f4504
Josh Brown <josh@roadfiresoftware.com>
2018-02-02 16:54:05 -0500

    create project structure

---
+++ NiceThingsTests/NiceThingsTests.swift
@@ -0,0 +1,24 @@
+//
+//  NiceThingsTests.swift
+//  NiceThingsTests
+//
+//  Created by Josh Brown on 2/2/18.
+//  Copyright © 2018 Roadfire Software. All rights reserved.
+//
+
+import XCTest
+@testable import NiceThings
+
+class NiceThingsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+        
+}
